f8938257fd2a5d8008ad96bcd9569c43ebb5ea15
Xjn <xjn@xjn.local>
2020-10-12 17:01:12 +0800

    v8 app_home使用模态框 + 图书名字去重复

--- app_home/urls.py
+++ app_home/urls.py
@@ -3,9 +3,10 @@ from app_home import views
 
 urlpatterns = [
     path('home/', views.home),
+    path('home_err/', views.home_err),
     path('publish/', views.publish),
     path('author/', views.author),
-    path('add_pub/', views.add_pub),
-    path('add_book/', views.add_book),
-    path('add_auth/', views.add_auth),
+    # path('add_pub/', views.add_pub),
+    # path('add_book/', views.add_book),
+    # path('add_auth/', views.add_auth),
 ]
--- app_home/views.py
+++ app_home/views.py
@@ -16,61 +16,33 @@ def home(request):
     if request.user.is_anonymous:
         return redirect('/app_auth/login/')
     else:
+        if request.method == "POST":
+            book_name = request.POST.get('book_name')
+            pub_id = request.POST.get('pub_id')
+            auth_id_list = request.POST.getlist('auth_id_list')
+            form = AddFormBook(request.POST)
+            # forms校验通过
+            if form.is_valid():
+                book_obj = Book.objects.create(book_name=book_name, pub_id=pub_id)
+                book_obj.auth.add(*auth_id_list)
+                # 重定向登录界面
+                return redirect('/app_home/home/')
+            else:
+                # forms校验不通过，继续注册
+                return render(request, 'home/home_err.html', locals())
         usr = request.user.username
         book_obj = Book.objects.all()
+        form = AddFormBook()
+        pub_obj = Publish.objects.all()
+        auth_obj = Author.objects.all()
         return render(request, "home/home.html", locals())
 
 
+def home_err(request):
+    return render(request, "home/home_err.html")
+
+
 def publish(request):
-    usr = request.user.username
-    pub_obj = Publish.objects.all()
-    return render(request, "home/publish.html", locals())
-
-
-def author(request):
-    usr = request.user.username
-    auth_obj = Author.objects.all()
-    return render(request, "home/author.html", locals())
-
-
-def add_book(request):
-    if request.method == "POST":
-        book_name = request.POST.get('book_name')
-        pub_id = request.POST.get('pub_id')
-        auth_id_list = request.POST.getlist('auth_id_list')
-        form = AddFormBook(request.POST)
-        # forms校验通过
-        if form.is_valid():
-            book_obj = Book.objects.create(book_name=book_name, pub_id=pub_id)
-            book_obj.auth.add(*auth_id_list)
-            # 重定向登录界面
-            return redirect('/app_home/home/')
-        else:
-            # forms校验不通过，继续注册
-            return render(request, 'home/add_book.html', locals())
-    form = AddFormBook()
-    pub_obj = Publish.objects.all()
-    auth_obj = Author.objects.all()
-    return render(request, "home/add_book.html", locals())
-
-
-def add_auth(request):
-    if request.method == "POST":
-        auth_name = request.POST.get('auth_name')
-        form = AddFormAuth(request.POST)
-        # forms校验通过
-        if form.is_valid():
-            Author.objects.create(auth_name=auth_name)
-            # 重定向登录界面
-            return redirect('/app_home/home/')
-        else:
-            # forms校验不通过，继续注册
-            return render(request, 'home/add_auth.html', locals())
-    form = AddFormAuth()
-    return render(request, "home/add_auth.html", locals())
-
-
-def add_pub(request):
     if request.method == "POST":
         pub_name = request.POST.get('pub_name')
         form = AddFormPub(request.POST)
@@ -78,9 +50,81 @@ def add_pub(request):
         if form.is_valid():
             Publish.objects.create(pub_name=pub_name)
             # 重定向登录界面
-            return redirect('/app_home/home/')
+            return redirect('/app_home/publish/')
         else:
             # forms校验不通过，继续注册
             return render(request, 'home/add_pub.html', locals())
+    usr = request.user.username
+    pub_obj = Publish.objects.all()
     form = AddFormPub()
-    return render(request, "home/add_pub.html", locals())
+    return render(request, "home/publish.html", locals())
+
+
+def author(request):
+    if request.method == "POST":
+        auth_name = request.POST.get('auth_name')
+        form = AddFormAuth(request.POST)
+        # forms校验通过
+        if form.is_valid():
+            Author.objects.create(auth_name=auth_name)
+            # 重定向登录界面
+            return redirect('/app_home/author/')
+        else:
+            # forms校验不通过，继续注册
+            return render(request, 'home/add_auth.html', locals())
+    usr = request.user.username
+    auth_obj = Author.objects.all()
+    form = AddFormAuth()
+    return render(request, "home/author.html", locals())
+
+# def add_book(request):
+#     if request.method == "POST":
+#         book_name = request.POST.get('book_name')
+#         pub_id = request.POST.get('pub_id')
+#         auth_id_list = request.POST.getlist('auth_id_list')
+#         form = AddFormBook(request.POST)
+#         # forms校验通过
+#         if form.is_valid():
+#             book_obj = Book.objects.create(book_name=book_name, pub_id=pub_id)
+#             book_obj.auth.add(*auth_id_list)
+#             # 重定向登录界面
+#             return redirect('/app_home/home/')
+#         else:
+#             # forms校验不通过，继续注册
+#             return render(request, 'home/add_book.html', locals())
+#     form = AddFormBook()
+#     pub_obj = Publish.objects.all()
+#     auth_obj = Author.objects.all()
+#     return render(request, "home/add_book.html", locals())
+
+
+# def add_auth(request):
+#     if request.method == "POST":
+#         auth_name = request.POST.get('auth_name')
+#         form = AddFormAuth(request.POST)
+#         # forms校验通过
+#         if form.is_valid():
+#             Author.objects.create(auth_name=auth_name)
+#             # 重定向登录界面
+#             return redirect('/app_home/home/')
+#         else:
+#             # forms校验不通过，继续注册
+#             return render(request, 'home/add_auth.html', locals())
+#     form = AddFormAuth()
+#     return render(request, "home/add_auth.html", locals())
+
+
+# def add_pub(request):
+#     if request.method == "POST":
+#         pub_name = request.POST.get('pub_name')
+#         form = AddFormPub(request.POST)
+#         # forms校验通过
+#         if form.is_valid():
+#             Publish.objects.create(pub_name=pub_name)
+#             # 重定向登录界面
+#             return redirect('/app_home/home/')
+#         else:
+#             # forms校验不通过，继续注册
+#             return render(request, 'home/add_pub.html', locals())
+#     form = AddFormPub()
+#     return render(request, "home/add_pub.html", locals())
